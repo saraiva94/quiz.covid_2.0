b584ecc752cac1d700b5a1c6eacb5d9facc9a0d2
Swamiy Saraiva <89426945+saraiva94@users.noreply.github.com>
2025-02-01 10:37:23 -0300

    Update script.js

--- ProjetoQuiz_Swamiy/script.js
+++ ProjetoQuiz_Swamiy/script.js
@@ -49,8 +49,7 @@ function resetState() {
     clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-        (answerButtonsElement.firstChild)
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
 }
 
